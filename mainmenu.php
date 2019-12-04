ba79ebe2f4357eff1797cd80bde35718aaf878dd
IloFelipeUnipe <55170305+IloFelipeUnipe@users.noreply.github.com>
2019-12-04 11:35:24 -0300

    Update mainmenu.php

--- mainmenu.php
+++ mainmenu.php
@@ -1,20 +1,20 @@
 <?php
 
-echo "<a href=\"listar.php\">Lista de Items<a/>";
+echo "<a href=\"listar.php\">Lista de Artefatos<a/>";
 
 echo "<br/>";
 
-echo "<a href=\"paginaValidacao.php\">Adicionar um Item<a/>";
+echo "<a href=\"paginaValidacao.php\">Adicionar um Artefato<a/>";
 
 echo "<br/>";
 
-echo "<a href=\"deletar.php\">Deletar um Item<a/>";
+echo "<a href=\"deletar.php\">Deletar um Artefato<a/>";
 
 echo "<br/>";
 
-echo "<a href=\"alterar.php\">Alterar um Item<a/>";
+echo "<a href=\"alterar.php\">Alterar um Artefato<a/>";
 
 echo "<br/>";
 
-echo "<a href=\"buscar.php\">Buscar um Item<a/>";
+echo "<a href=\"buscar.php\">Buscar um Artefato<a/>";
 ?>
