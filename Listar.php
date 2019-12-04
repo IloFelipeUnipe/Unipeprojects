03bd1b101140379188205597867bbd541e683f3a
IloFelipeUnipe <55170305+IloFelipeUnipe@users.noreply.github.com>
2019-12-04 11:33:25 -0300

    Update Listar.php

--- Listar.php
+++ Listar.php
@@ -6,11 +6,13 @@
     }else{
         echo "Erro na Conexao!";
     }
-    $produtos = listarTodos();
-        for($i = 0; $i < count($produtos); $i++){
-        echo "Id: " . $produtos[$i]["id"] . "<br/>";
-        echo "Nome: " . $produtos[$i]["nome"] . "<br/>";
-        echo "preco: " . $produtos[$i]["preco"] . "<br/> <br/>";
-    }
+    $artefatos = listarTodos();
+    for($i = 0; $i < count($artefatos); $i++){
+    echo "Id: " . $artefatos[$i]["id"] . "<br/>";
+    echo "Nome: " . $artefatos[$i]["nome"] . "<br/>";
+    echo "Tipo: " . $artefatos[$i]["tipo"] . "<br/>";
+    echo "Atributos: " . $artefatos[$i]["atributos"] . "<br/> <br/>";
+
+}
     
 ?>
