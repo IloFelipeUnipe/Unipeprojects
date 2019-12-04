943b381d7f332b6d0fd52410cafa04d4f35f4372
IloFelipeUnipe <55170305+IloFelipeUnipe@users.noreply.github.com>
2019-12-04 11:34:25 -0300

    Update alterar.php

--- alterar.php
+++ alterar.php
@@ -2,30 +2,34 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Alterando um item</title>
+    <title>Alterando um Artefato</title>
 </head>
 <body>
 <?php
     include 'operacoes.php';
     if ($_SERVER["REQUEST_METHOD"]== "POST"){
-        alterar($_POST["id"],$_POST["nome"],$_POST["preco"]);
+        alterar($_POST["id"],$_POST["nome"],$_POST["tipo"],$_POST["atributos"]);
     }
-    $produtos = listarTodos();
-    for($i = 0; $i < count($produtos); $i++){
-    echo "Id: " . $produtos[$i]["id"] . "<br/>";
-    echo "Nome: " . $produtos[$i]["nome"] . "<br/>";
-    echo "preco: " . $produtos[$i]["preco"] . "<br/> <br/>";
+    $artefatos = listarTodos();
+    for($i = 0; $i < count($artefatos); $i++){
+    echo "Id: " . $artefatos[$i]["id"] . "<br/>";
+    echo "Nome: " . $artefatos[$i]["nome"] . "<br/>";
+    echo "Tipo: " . $artefatos[$i]["tipo"] . "<br/>";
+    echo "Atributos: " . $artefatos[$i]["atributos"] . "<br/> <br/>";
+
 }
 
 ?>
-    <h1>Alterando um item</h1>
-    <form id="produtosdb" action="alterar.php" method="POST" onsubmit="return validar()">
+    <h1>Alterando um Artefato</h1>
+    <form id="atributosdb" action="alterar.php" method="POST" onsubmit="return validar()">
         id
         <input type="text" name="id"><br><br>
         nome
         <input type="text" name="nome"><br><br>
-        preco
-        <input type="text" name="preco"><br><br>
+        tipo
+        <input type="text" name="tipo"><br><br>
+        atributos
+        <input type="text" name="atributos"><br><br>
         <button>alterar</button>
     </form>
 
@@ -34,7 +38,7 @@
 
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
     <script>
-        let form = document.querySelector("#produtosdb");
+        let form = document.querySelector("#atributosdb");
         function validar(){
             if(form.nome.value == false){
                 Swal.fire(
@@ -44,7 +48,7 @@
                 )
                 return false;
             }
-            if(form.preco.value == false){
+            if(form.tipo.value == false){
                 alert("Informe uma quantidade")
                 return false;
             }
